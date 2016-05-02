d1824ff4a990f1126871c910036ca93b3d30cee8
ericoporto <ericoporto2008@gmail.com>
2016-05-02 15:27:00 -0300

    updating

--- ShellOut.py
+++ ShellOut.py
@@ -10,12 +10,12 @@ Rob Antonishen
 Version:
 0.8 better complex external program handling
 0.7 fixed file save bug where all files were png regardless of extension
-0.6 modified to allow for a returned layer that is a different size 
+0.6 modified to allow for a returned layer that is a different size
    than the saved layer for
   0.5 file extension parameter in program list.
 0.4 modified to support many optional programs.
 
-this script is modelled after the mm extern LabCurves trace plugin 
+this script is modelled after the mm extern LabCurves trace plugin
 by Michael Munzert http://www.mm-log.com/lab-curves-gimp
 
 and thanks to the folds at gimp-chat has grown a bit ;)
@@ -48,14 +48,14 @@ def nikWine(name, ndir = None, exe = None):
   if ndir is None:
     ndir = name
   if exe is None:
-    exe = name  
+    exe = name
   return [name,
-          ["wine", os.path.expanduser("~/win32/drive_c/Program Files/Google/Nik Collection/" +
+          ["wine", os.path.expanduser("~/.wine/drive_c/Program Files/Google/Nik Collection/" +
                                       os.path.join(ndir, exe + ".exe"))],
           "tif"]
 
 def nikWineHDRHook(tmpfile, ext):
-  shutil.move(os.path.expandvars("$HOME/win32/drive_c/users/$USER/My Documents/" + TEMP_FNAME + "_HDR." + ext),
+  shutil.move(os.path.expandvars("$HOME/.wine/drive_c/users/$USER/My Documents/" + TEMP_FNAME + "_HDR." + ext),
               tmpfile)
 
 #program list function (globals are evil)
@@ -64,7 +64,7 @@ def listcommands(option=None):
   # Insert additonal shell command into this list.  They will show up in the drop menu in this order.
   # Use the syntax:
   # ["Menu Label", "command", "ext"]
-  # 
+  #
   # Where what gets executed is command fileame so include and flags needed in the command.
   programlist = [
   nikWine("Analog Efex Pro 2"),
@@ -77,7 +77,7 @@ def listcommands(option=None):
   nikWine("Viveza 2"),
   ["",[],""]
   ]
-  
+
   if option == None: # no parameter return menu list, otherwise return the appropaiate array
     menulist = []
     for i in programlist:
@@ -86,11 +86,11 @@ def listcommands(option=None):
     return menulist
   else:
     return programlist[option]
-	
+
 
 def plugin_main(image, drawable, visible, command):
   pdb.gimp_image_undo_group_start(image)
-  
+
   # Copy so the save operations doesn't affect the original
   if visible == 0:
     # Save in temporary.  Note: empty user entered file name
@@ -114,10 +114,10 @@ def plugin_main(image, drawable, visible, command):
   pdb.gimp_image_undo_disable(tempimage)
 
   tempdrawable = pdb.gimp_image_get_active_layer(tempimage)
-  
+
   #get the program to run and filetype.
   progtorun = listcommands(command)
-  
+
   # Use temp file names from gimp, it reflects the user's choices in gimp.rc
   # change as indicated if you always want to use the same temp file name
   # tempfilename = pdb.gimp_temp_name(progtorun[2])
@@ -145,7 +145,7 @@ def plugin_main(image, drawable, visible, command):
     newlayer2 = pdb.gimp_file_load_layer(tempimage, tempfilename)
   except:
     RuntimeError
-	
+
   tempimage.add_layer(newlayer2,-1)
   buffer = pdb.gimp_edit_named_copy(newlayer2, "ShellOutTemp")
 
@@ -159,14 +159,14 @@ def plugin_main(image, drawable, visible, command):
     temp.translate((tempdrawable.width-newlayer2.width)/2,(tempdrawable.height-newlayer2.height)/2)
 
   pdb.gimp_buffer_delete(buffer)
-  pdb.gimp_edit_clear(temp)	
+  pdb.gimp_edit_clear(temp)
   pdb.gimp_floating_sel_anchor(sel)
 
   #load up old selection
   if hassel:
     pdb.gimp_selection_load(savedsel)
     image.remove_channel(savedsel)
-  
+
   # cleanup
   os.remove(tempfilename)  # delete the temporary file
   gimp.delete(tempimage)   # delete the temporary image
@@ -184,7 +184,7 @@ register(
         "Copyright 2011 Rob Antonishen",
         "2011",
         "<Image>/Script-Fu/ShellOut...",
-        "RGB*, GRAY*", 
+        "RGB*, GRAY*",
         [ (PF_RADIO, "visible", "Layer:", 1, (("new from visible", 1),("current layer",0))),
           (PF_OPTION,"command",("Program:"),0,listcommands())
         ],
